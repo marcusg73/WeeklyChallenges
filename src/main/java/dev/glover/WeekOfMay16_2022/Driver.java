9ab12579cac67eb4fd5d45fb94495995ad41b1fa
Marcus Glover <marcusg73@yahoo.com>
2022-05-17 13:15:24 -0400

    Added comments

--- src/main/java/dev/glover/WeekOfMay16_2022/Driver.java
+++ src/main/java/dev/glover/WeekOfMay16_2022/Driver.java
@@ -28,32 +28,39 @@ public class Driver
 
     private LinkedList<Integer> convertToReversedLinkedList(Integer num)
     {
+        // Convert number into a String for looping
         String strNum = num.toString();
 
+        // Create a LinkedList to hold the digits of the number
         LinkedList<Integer> reversedNum = new LinkedList<>();
 
+        // Loop through number and add them to the list
         for (int i = 0; i < strNum.length(); i++)
         {
             reversedNum.addFirst(Integer.parseInt(String.valueOf(strNum.charAt(i))));
         }
 
+        // Return the list
         return reversedNum;
     }
 
     private LinkedList<Integer> SumLists(LinkedList<Integer> num1, LinkedList<Integer> num2, Boolean areReversed)
     {
-        Integer sum;
-        int n1 = 0, n2 = 0;
-        int digitPlace = 1;
+        Integer sum;            // Holds sum of the numbers
+        int n1 = 0, n2 = 0;     // References to value of each number
+        int digitPlace = 1;     // Keeps track of digit place value when looping
 
+        // Check if the numbers are reversed or not
         if (areReversed)
         {
+            // Loop through the first number while converting into an int for addition operation
             for (int i = 0; i < num1.size(); i++)
             {
                 n1 += (digitPlace * num1.get(i));
                 digitPlace *= 10;
             }
 
+            // Loop through the second number while converting into an int for addition operation
             digitPlace = 1;
             for (int i = 0; i < num2.size(); i++)
             {
@@ -61,18 +68,20 @@ public class Driver
                 digitPlace *= 10;
             }
 
-            sum = n1 + n2;
+            sum = n1 + n2;                              // Add numbers
 
-            return convertToReversedLinkedList(sum);
+            return convertToReversedLinkedList(sum);    // Return the reversed LinkedList of the sum
         }
         else
         {
+            // Loop through the first number while converting into an int for addition operation
             for (int i = num1.size() - 1; i >= 0; i--)
             {
                 n1 += (digitPlace * num1.get(i));
                 digitPlace *= 10;
             }
 
+            // Loop through the second number while converting into an int for addition operation
             digitPlace = 1;
             for (int i = num2.size() - 1; i >= 0; i--)
             {
@@ -80,18 +89,19 @@ public class Driver
                 digitPlace *= 10;
             }
 
-            sum = n1 + n2;
+            sum = n1 + n2;                                      // Add numbers
 
-            String strNum = sum.toString();
+            String strNum = sum.toString();                     // Convert sum into a String for looping
 
-            LinkedList<Integer> sumList = new LinkedList<>();
+            LinkedList<Integer> sumList = new LinkedList<>();   // Create a LinkedList to hold the digits of the sum
 
+            // Convert sum into a LinkedList
             for (int i = 0; i < strNum.length(); i++)
             {
                 sumList.addLast(Integer.parseInt(String.valueOf(strNum.charAt(i))));
             }
 
-            return sumList;
+            return sumList;     // Return the LinkedList of the sum
         }
     }
 }
